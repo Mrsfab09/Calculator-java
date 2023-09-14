21b83090b2a20dd420c50313ac4116146e426b5e
Fabian <ytfabiskora07@gmail.coml>
2023-09-14 22:27:33 +0200

    add files calculator

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double wynik = 0;
+
+        System.out.println("--Calculator--\n");
+
+        System.out.println("\t[+] Dodawanie");
+        System.out.println("\t[-] Odejmowanie");
+        System.out.println("\t[*] Mnozenie");
+        System.out.println("\t[/] Dzielenie");
+        System.out.println("\t[Q] Wyjscie\n");
+
+        System.out.println("Wybierz: ");
+        char chooseOption = scanner.next().charAt(0);
+        //TODO Dodanie funkcji ktora umozliwi uzytkownikowi wpisanie wiecej liczb niz dwie
+        //TODO albo bedzie sam wybieral ile liczb chce dodac
+        //TODO albo nie bedzie to okreslone
+
+//        switch
+//          code
+
+    }
+}
